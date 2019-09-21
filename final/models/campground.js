478f4bb9dae1c348d5aa1ab96e7e035a585e5d00
rosariogomezcor <rosariogomez639@gmail.com>
2019-09-21 20:06:50 -0300

    add dynamic price feature

--- final/models/campground.js
+++ final/models/campground.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 //MONGOOSE SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String, 
+	price: String,
 	image: String, 
 	description: String, 
 	author: {
